c3062ce868960603f3de9d97fae7a68fc2640a25
Santiago <andressasi@unisabana.edu.co>
2025-02-08 19:44:13 -0500

    first commit

---
+++ FrontendK8s/Pages/Index.cshtml.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
+using System.Text.Json;
+
+namespace FrontendK8s.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly IHttpClientFactory httpClientFactory;
+    private readonly IConfiguration _configuration;
+
+    [BindProperty]
+    public string Nombre {get; set;}
+    public List<string> Nombres {get; set;} = new List<string>();
+
+    public IndexModel(IHttpClientFactory clientFactory, IConfiguration configuration){
+        httpClientFactory = clientFactory;
+        _configuration = configuration;
+    }
+
+    public async Task OnGetAsync(){
+        await CargarNombres(); //Espera para cargar los nombres del endpoint
+    }
+
+    public async Task<IActionResult> OnPostAsync(){
+        if(string.IsNullOrWhiteSpace(Nombre)){
+            return Page();
+        }
+        var client = httpClientFactory.CreateClient();
+        var apiUrl = _configuration["ApiSettings:BaseUrl"];
+
+        try{
+            var content = new StringContent(
+                JsonSerializer.Serialize(new {nombre = Nombre}),
+                Encoding.UTF8,
+                "application/json"
+                );
+            
+            var response = await client.PostAsync($"{apiUrl}/nombres", content);
+            response.EnsureSuccessStatusCode();
+
+            await CargarNombres();
+        } catch (Exception ex){
+            ModelState.AddModelError(string.Empty, $"Error {ex.Message} guardando nombre");
+        }
+
+        return Page();
+    }
+
+    private async Task CargarNombres(){
+        var client = httpClientFactory.CreateClient();
+        var apiUrl = _configuration["ApiSettings:BaseUrl"];
+
+        try
+        {
+            var response = await client.GetAsync($"{apiUrl}/Nombres");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            Nombres = JsonSerializer.Deserialize<List<string>>(content);
+        }
+        catch (Exception ex)
+        {
+           ModelState.AddModelError(string.Empty, $"Error {ex.Message} al cargar los nombres.");
+        }
+    }
+}
---
+++ FrontendK8s/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Indica que la aplicación escuche en el puerto 80
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
